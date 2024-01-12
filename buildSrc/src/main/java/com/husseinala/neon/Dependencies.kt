698612fcfb93013082eee23f49b02dc36b497966
Hussein Aladeen <hussein@fueled.com>
2024-01-12 00:04:28 +0000

    Prepare version 1.0.0

--- buildSrc/src/main/java/com/husseinala/neon/Dependencies.kt
+++ buildSrc/src/main/java/com/husseinala/neon/Dependencies.kt
@@ -52,7 +52,7 @@ object Deps {
         const val TRANSFORMATIONS = "jp.wasabeef:picasso-transformations:2.4.0"
     }
 
-    const val MAVEN_PUBLISH = "com.vanniktech:gradle-maven-publish-plugin:0.13.0"
+    const val MAVEN_PUBLISH = "com.vanniktech:gradle-maven-publish-plugin:0.27.0"
 
     const val GLIDE = "com.github.bumptech.glide:glide:4.12.0"
 }
